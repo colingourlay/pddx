6a105dc3dcff63619761d6c954848a20b690a667
Colin Gourlay <colin@colin-gourlay.com>
2023-03-12 20:30:34 +1000

    Add aliases for commands

--- bin/playdate.mjs
+++ bin/playdate.mjs
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 
-const [command] = process.argv.slice(2);
+const ALIASES = {
+	b: "build",
+	c: "clean",
+	d: "dev",
+	dd: "dev-with-device",
+	i: "install",
+	il: "install-and-launch",
+	l: "launch",
+	s: "simulate",
+};
 
+const [commandOrAlias] = process.argv.slice(2) || "build";
+const command = ALIASES[commandOrAlias] || commandOrAlias;
 let commandModule;
 
 try {
-	commandModule = await import(`../lib/commands/${command || "build"}.mjs`);
+	commandModule = await import(`../lib/commands/${command}.mjs`);
 } catch (e) {
 	console.error("Command not recognized");
 }
